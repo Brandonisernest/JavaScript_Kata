157802c15d4ee37c1d861b6c45cd95526bf7b277
Brandonisernest <48809099+Brandonisernest@users.noreply.github.com>
2021-10-04 19:51:11 -0400

    Add files via upload

--- MovieDatabase_PracticeDOM/assets/scripts/app.js
+++ MovieDatabase_PracticeDOM/assets/scripts/app.js
@@ -4,12 +4,48 @@ const backdrop = document.getElementById('backdrop');
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll('input');
+const entryTextSection = document.getElementById('entry-text');
+//store movie info
+
+const movies = [];
 
 ////Functions
+
+const updateUI = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = 'block';
+  } else {
+    entryTextSection.style.display = 'none';
+  }
+};
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+  const newMovieElement = document.createElement('li');
+  newMovieElement.className = 'movie-element';
+  newMovieElement.innerHTML = `
+  <div class="movie-element__image">
+    <img src="${imageUrl}" alt="${title}">
+  </div>
+  <div class="movie-element__info">
+    <h2>${title}</h2>
+    <p>${rating}/5 stars</p>
+  </div>
+  `;
+
+  const listRoot = document.getElementById('movie-list');
+  listRoot.append(newMovieElement);
+};
+
 const toggleMovieModal = () => {
   //   addMovieModal.className = 'modal card';
   addMovieModal.classList.toggle('visible'); //adds/remove ".visible" to the class of element with ID: 'add-modal'
   toggleBackdrop();
+};
+
+const clearMovieInput = () => {
+  for (const usrInput of userInputs) {
+    usrInput.value = '';
+  }
 };
 
 const toggleBackdrop = () => {
@@ -22,20 +58,10 @@ const backdropClickHandler = () => {
 
 const cancelAddMovieHandler = () => {
   toggleBackdrop();
+  clearMovieInput();
 };
 
 const addMovieHandler = () => {
-  //   const titleInput = document.getElementById('title');
-  //   const urlInput = document.getElementById('image-url');
-  //   const ratingInput = document.getElementById('rating');
-  //   let ratingValue = ratingInput.value;
-
-  //   if (ratingValue < ratingInput.min || ratingValue > ratingInput.max) {
-  //     alert('You did not enter a proper rating!');
-  //   } else {
-  //     alert(`${ratingValue}`);
-  //   }
-  //
   const titleValue = userInputs[0].value;
   const imageUrlValue = userInputs[1].value;
   const ratingValue = userInputs[2].value;
@@ -48,9 +74,20 @@ const addMovieHandler = () => {
     ratingValue > 5
   ) {
     alert('Please enter valid values!');
-  } else {
-      alert('Noice!');
+    return;
   }
+
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  };
+
+  movies.push(newMovie);
+  toggleMovieModal();
+  clearMovieInput();
+  renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+  updateUI();
 };
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
